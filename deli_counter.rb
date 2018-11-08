cd4c3b730bc27d9e52a3ff595094231a5a411fc6
Matthew Sherman <msherman0801@gmail.com>
2018-11-08 00:42:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,3 @@
-
 def line(katz_deli)
   newLine = ["The line is currently:"]
   katz_deli.each_with_index do |name, index|
@@ -15,13 +14,13 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
   if katz_deli.length > 0 
     puts "Currently serving #{katz_deli[0]}."
-    katz_deli.delete_at(0)
+    katz_deli.delete(katz_deli.first)
   else
     puts "There is nobody waiting to be served!"
   end
